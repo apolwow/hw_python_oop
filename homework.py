a1bc5324c02f463714d225595d1eae9cb835f6c8
Alexey Polozhentsev <apolwow@gmail.com>
2021-06-07 12:05:04 +0500

    code changes in accordance with the reviewer comments

--- homework.py
+++ homework.py
@@ -2,12 +2,12 @@ import datetime as dt
 
 
 class Record:
-    '''Класс Record содержит данные о денежной сумме или количестве
+    """Класс Record содержит данные о денежной сумме или количестве
     килокалорий, дате создания записи и поясняющих комментариях.
-    '''
+    """
 
     def __init__(self, amount, comment, date=None):
-        '''Метод __init__ инициализирует атрибуты.'''
+        """Метод __init__ инициализирует атрибуты."""
         self.amount = amount
         self.comment = comment
         if date is None:
@@ -17,53 +17,54 @@ class Record:
 
 
 class Calculator:
-    '''Класс Calculator содержит данные о дневном лимите трат/калорий,
+    """Класс Calculator содержит данные о дневном лимите трат/калорий,
     которые задал пользователь. Пустой список для хранения записей.
-    '''
+    """
     def __init__(self, limit):
-        '''Метод __init__ инициализирует атрибуты.'''
+        """Метод __init__ инициализирует атрибуты."""
         self.limit = limit
         self.records = []
-        self.today = dt.date.today()
-        self.week_later = self.today - dt.timedelta(days=7)
 
     def add_record(self, record):
-        '''Метода add_record принимает объект класса Record и добавляет
+        """Метода add_record принимает объект класса Record и добавляет
         его в конец списка records.
-        '''
+        """
         self.records.append(record)
 
     def get_today_stats(self):
-        '''Метод get_today_stats считает сколько каллорий
+        """Метод get_today_stats считает сколько каллорий
         съедено/сколько денег потрачено за текущий день.
-        '''
+        """
+        today = dt.date.today()
         return sum(
             record.amount for record in self.records
-            if record.date == self.today)
+            if record.date == today)
 
     def get_week_stats(self):
-        '''Метод get_week_stats считает количество полученных
+        """Метод get_week_stats считает количество полученных
         каллорий/потраченых денег за последние 7 дней.
-        '''
+        """
+        today = dt.date.today()
+        week_later = today - dt.timedelta(days=6)
         return sum(
             record.amount for record in self.records
-            if self.week_later <= record.date <= self.today)
+            if week_later <= record.date <= today)
 
     def get_remaining_limit(self):
-        '''Метод get_remaining_limit считает оставшиеся
+        """Метод get_remaining_limit считает оставшиеся
         каллории/денеги.
-        '''
+        """
         return self.limit - self.get_today_stats()
 
 
 class CaloriesCalculator(Calculator):
-    '''Подкласс CaloriesCalculator для расчета каллорий.
+    """Подкласс CaloriesCalculator для расчета каллорий.
     Содержит данные надкласса.
-    '''
+    """
     def get_calories_remained(self):
-        '''Метод get_calories_remained проверяет сколько каллорий
+        """Метод get_calories_remained проверяет сколько каллорий
         получено и возвращает соответсвующие рекоммендации.
-        '''
+        """
         calories_remained = self.get_remaining_limit()
         if calories_remained > 0:
             return (
@@ -73,31 +74,32 @@ class CaloriesCalculator(Calculator):
 
 
 class CashCalculator(Calculator):
-    '''Подкласс CashCalculator для учета финансов.
+    """Подкласс CashCalculator для учета финансов.
     Содержит данные надкласса.
-    '''
+    """
     # Константы для соответсвующей валюты
     RUB_RATE = 1.0
     USD_RATE = 60.0
     EURO_RATE = 70.0
 
     def get_today_cash_remained(self, currency):
-        '''Метод get_today_cash_remained денежного калькулятора
+        """Метод get_today_cash_remained денежного калькулятора
         принимает на вход код валюты и возвращает дневной баланс.
-        '''
-        currencies = {
-            'rub': ('руб', self.RUB_RATE),
-            'usd': ('USD', self.USD_RATE),
-            'eur': ('Euro', self.EURO_RATE)
-        }
-        cash_remained = self.get_remaining_limit()
-        if currency not in currencies:
-            return f'Указана неправильная валюта - {currency}'
-        if cash_remained == 0:
-            return 'Денег нет, держись'
-        cur_name, rate = currencies.get(currency)
-        cash_remained = round(cash_remained / rate, 2)
-        if cash_remained > 0:
-            return f'На сегодня осталось {cash_remained} {cur_name}'
-        cash_remained = abs(cash_remained)
-        return f'Денег нет, держись: твой долг - {cash_remained} {cur_name}'
+        """
+        try:
+            currencies = {
+                'rub': ('руб', self.RUB_RATE),
+                'usd': ('USD', self.USD_RATE),
+                'eur': ('Euro', self.EURO_RATE)
+            }
+            cash_remained = self.get_remaining_limit()
+            if cash_remained == 0:
+                return 'Денег нет, держись'
+            cur_name, rate = currencies.get(currency)
+            cash_remained = round(cash_remained / rate, 2)
+            if cash_remained > 0:
+                return f'На сегодня осталось {cash_remained} {cur_name}'
+            cash_remained = abs(cash_remained)
+            return f'Денег нет, держись: твой долг-{cash_remained} {cur_name}'
+        except TypeError:
+            print('Указанный код валюты не поддерживается.')
